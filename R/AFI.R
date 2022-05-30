b9159ee65c51275c56fdeff93d1837e0b1347f43
Jacob Greer <jacobmgreer@users.noreply.github.com>
2022-05-29 20:39:51 -0400

    updates

--- R/AFI.R
+++ R/AFI.R
@@ -45,6 +45,7 @@ for (i in 1:nrow(AFIlineup)) {
     data.frame(
       FilmID = AFIlineup$FilmID[i],
       TheatreID = AFIlineup$TheatreID[i],
+      SessionID = gsub('^.*SessionId=\\s*|\\s*&vis.*$', '', link %>% html_nodes(.,'.film-showtimes .session .session-times a') %>% html_attr("href")),
       ScreenTitle = AFIlineup$Title[i],
       ScreenTrailer = AFIlineup$Trailer[i],
       ScreenPoster = AFIlineup$Poster[i],
@@ -71,6 +72,30 @@ for (i in 1:nrow(showtimes)) {
   theatres <- rbind(theatres, page)
 }
 
-showtimes <- left_join(showtimes, theatres, by = c("TheatreID", "ScreenTicketLink"))
+showtimes <- left_join(showtimes, theatres, by = c("TheatreID", "ScreenTicketLink")) %T>%
+  write.csv(., "datasets/AFI-Silver/Showtimes.csv", row.names = FALSE)
+
+ScreeningCal <- function(showtimes2) {
+  ics_header <- readLines("ics_template/template_header.ics", warn = F)
+  ics_body <- readLines("ics_template/template_body.ics", warn = F)
+  ics_footer <- readLines("ics_template/template_footer.ics", warn = F)
+
+  ics_events <- ""
+  for(i in 1:nrow(showtimes2)) {
+    ics_body <- str_replace(ics_body, "UID:.*", paste0("UID:", showtimes2$SessionID[i]))
+    ics_body <- str_replace(ics_body, "DTSTAMP:.*", paste0("DTSTAMP:", format(now(tzone = "EST"), "%Y%m%dT%H%M%S")))
+    ics_body <- str_replace(ics_body, "DTSTART:.*", paste0("DTSTART:", format(ymd_hms(showtimes2$ScreenTimestamp[i]), "%Y%m%dT%H%M%S")))
+    ics_body <- str_replace(ics_body, "DTEND:.*", paste0("DTEND:", format(ymd_hms(showtimes2$ScreenTimestamp[i]) + minutes(str_remove(showtimes2$ScreenRuntime[i],' Minutes')), "%Y%m%dT%H%M%S")))
+    ics_body <- str_replace(ics_body, "SUMMARY:.*", paste0("SUMMARY:", iconv(showtimes2$ScreenTitle[i], "UTF-8", "ASCII", sub = "")))
+    ics_body <- str_replace(ics_body, "LOCATION:.*", paste0("LOCATION:", showtimes2$ScreenTheatre[i]))
+    ics_body <- str_replace(ics_body, "DESCRIPTION:.*", paste0("DESCRIPTION:http:", showtimes2$ScreenTicketLink[i]))
+    ics_events <- append(ics_events, ics_body)
+  }
+  ics_events <- append(ics_header, ics_events)
+  ics_events <- append(ics_events, ics_footer)
+  return(ics_events)
+}
+
+
 
 rm(page,link,i)
